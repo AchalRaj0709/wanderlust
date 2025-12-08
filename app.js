a77fe5e0eadb027c45506ca57380c4e835e66df8
AchalRaj0709 <rajachalp782@gmail.com>
2025-12-09 02:31:25 +0530

    Added Delete Route

--- app.js
+++ app.js
@@ -67,6 +67,14 @@ app.put("/listings/:id",async (req,res)=>{
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
 });
+
+//Delete Route
+app.delete("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
 // app.get("/testlisting",async (req,res)=>{
 //     let samplisting=new Listing({
 //         title:"My New Villa",
